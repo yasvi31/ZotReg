e3e67c3f5ee862e1188eeeddfc4af61ca002c807
Yasvi Patel <patelya@uci.edu>
2020-12-16 15:26:29 -0800

    Study list basic UI done + add component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { StudyListComponent } from './components/study-list/study-list.component';
+import { AddComponent } from './components/add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    StudyListComponent
+    StudyListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/study-list/study-list.component.ts
+++ src/app/components/study-list/study-list.component.ts
@@ -7,13 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudyListComponent implements OnInit {
   courselist:string[][];
+  message:string;
 
   constructor() {
     //using default data
     this.courselist=[['34270', 'COMPSCI', '163'],['34300', 'COMPSCI', '167'],['35780', 'I&C', 'SCI'],['35980', 'IN4MATX', '134']];
+    this.message = "";
   }
 
   ngOnInit(): void {
+  }
+
+  change(courseCode:string): void{
+    this.message = "Grade Changed to P/NP for "+courseCode;
+  }
+
+  drop(courseCode:string): void{
+    this.message = "Class Dropped Successfully";
+    this.courselist = this.courselist.filter(x => x[0] != courseCode);
+  }
+
+  isMessage(): Booleen{
+    return this.message != "";
   }
 
 }
