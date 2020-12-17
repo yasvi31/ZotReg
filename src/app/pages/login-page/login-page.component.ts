014dab435076940e23245099df4f7721097e1b3c
Yasvi Patel <patelya@uci.edu>
2020-12-16 18:10:41 -0800

    Login Page skeleton added

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  username:string;
+  password:string;
 
-  constructor() { }
+  constructor() {
+    this.username="";
+    this.password="";
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.username,this.password);
   }
 
 }
