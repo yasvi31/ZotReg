3e62ea315056d722cbe2ca150caee0cb8df05c34
Yasvi Patel <patelya@uci.edu>
2020-12-16 18:39:17 -0800

    Course service added

--- src/app/components/enroll-list/enroll-list.component.ts
+++ src/app/components/enroll-list/enroll-list.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enroll-list.component.css']
 })
 export class EnrollListComponent implements OnInit {
+  enroll:string[][];
 
-  constructor() { }
+  constructor() {
+  this.enroll=[['34270', 'COMPSCI', '163'],['34300', 'COMPSCI', '167'],['35780', 'I&C', 'SCI'],['35980', 'IN4MATX', '134']];
+ }
 
   ngOnInit(): void {
   }
 
+  removeCourse(courseCode:string){
+    this.enroll=this.enroll.filter(x => x[0] != courseCode);
+  }
 }
