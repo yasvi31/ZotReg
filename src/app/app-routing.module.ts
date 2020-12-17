51cc376c565036b57b1f382a128d274134a56701
Yasvi Patel <patelya@uci.edu>
2020-12-16 17:40:48 -0800

    Links to enroll-page and login-page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { EnrollPageComponent } from './pages/enroll-page/enroll-page.component';
 
 const routes: Routes = [
+  {path:'enroll',component:EnrollPageComponent},
+  {path:'login',component:LoginPageComponent},
   {path:'',component:HomePageComponent}
 ];
 
